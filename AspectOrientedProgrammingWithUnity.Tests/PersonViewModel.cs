e1f9c9a5f683b001d66eed172249d2238554a0fa
jalalx <sjamini@gmail.com>
2015-03-27 00:27:58 +0430

    init

---
+++ AspectOrientedProgrammingWithUnity.Tests/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+namespace AspectOrientedProgrammingWithUnity.Tests
+{
+    public class PersonViewModel : BaseViewModel
+    {
+        public string FullName { get; [Notify]set; }
+        public DateTime Birthday { get; [Notify(Order = 1)][Notify(PropertyName = "Age", Order = 2)]set; }
+        public int Age { get { return DateTime.Now.Year - Birthday.Year; } }
+    }
+}
---
+++ AspectOrientedProgrammingWithUnity.Tests/PropertyChangesUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspectOrientedProgrammingWithUnity.Tests
+{
+    [TestClass]
+    public class PropertyChangesUnitTest
+    {
+        [TestMethod]
+        public void CheckPropertyChangedGetsCalledForPropertiesInAttributeWay()
+        {
+            using (var container = new UnityContainer())
+            {
+                container.AddNewExtension<Interception>();
+                container.RegisterType<PersonViewModel>(
+                    new Interceptor<TransparentProxyInterceptor>(),
+                    new InterceptionBehavior<PolicyInjectionBehavior>());
+
+                var propertyChanged = false;
+                var propertyNames = new List<string>();
+                var viewModel = container.Resolve<PersonViewModel>();
+
+                viewModel.PropertyChanged += (sender, e) =>
+                {
+                    propertyChanged = true;
+                    propertyNames.Add(e.PropertyName);
+                };
+
+                viewModel.FullName = "new value!";
+                Assert.IsTrue(propertyChanged, "PropertyChanged not called!");
+                Assert.IsTrue(propertyNames.Contains("FullName"));
+                propertyNames.Clear();
+
+                viewModel.Birthday = new DateTime(1990, 1, 1);
+                Assert.IsTrue(propertyChanged, "PropertyChanged not called!");
+                Assert.IsTrue(propertyNames.Contains("Birthday"));
+                Assert.IsTrue(propertyNames.Contains("Age"));
+            }
+        }
+    }
+}
